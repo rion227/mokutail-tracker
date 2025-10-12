704a7fed23e13280b4e095adaa8925ff2764c7c9
rion227 <rororion227@gmail.com>
2025-10-13 07:43:26 +0900

    set base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 👉 GitHub Pages のパス（ユーザー名/リポジトリ名）
+  base: '/mokutail-tracker/',
 })
